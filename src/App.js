e6f290bab966c3520640014c8937dbd19217f653
Vicente G. Reyes <icenreyes@icloud.com>
2020-07-14 16:02:53 +0800

    5.6 finished

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Login from './components/Login'
+import AppHeader from './components/AppHeader'
 import Eventlite from './components/Eventlite'
 
 const currentUser = function() {
@@ -11,6 +12,7 @@ const currentUser = function() {
 function App() {
   return (
     <div className="App">
+      <AppHeader />
       {currentUser() ? <Eventlite /> : <Login />}
     </div>
   )
