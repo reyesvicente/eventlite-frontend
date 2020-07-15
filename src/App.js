f22f467a79869e516f9abf7aefdf57cf934deb13
Vicente G. Reyes <icenreyes@icloud.com>
2020-07-16 00:41:11 +0800

    end of 6.4

--- src/App.js
+++ src/App.js
@@ -3,19 +3,26 @@ import Login from './components/Login'
 import Signup from './components/Signup'
 import AppHeader from './components/AppHeader'
 import Eventlite from './components/Eventlite'
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom"
 
-const currentUser = function() {
-  const user = localStorage.getItem('user')
-  console.log(user)
-  return(user)
-}
 
 function App() {
+  const currentUser = localStorage.getItem('user')
   return (
-    <div className="App">
-      <AppHeader />
-      {currentUser() ? <Eventlite /> :<><Login /> <Signup /></>}
-    </div>
+    <Router>
+      <Route path="/">
+        <AppHeader />
+      </Route>
+      <Route exact path="/">
+        <Eventlite />
+      </Route>
+      <Route exact path="/login">
+        {currentUser ? <Redirect to="/" /> : <Login />}
+      </Route>
+      <Route exact path="/signup">
+        {currentUser ? <Redirect to="/" /> : <Signup />}
+      </Route>
+    </Router>
   )
 }
 
