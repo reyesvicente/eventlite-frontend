9a904ed6b6bfd0cea722a32c51366946deafeee8
Vicente G. Reyes <icenreyes@icloud.com>
2020-07-25 21:24:01 +0800

    lesson 9.2 done

---
+++ src/__tests__/EventSummary.test.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import EventSummary from '../components/EventSummary'
+
+it('renders event title', () => {
+    const { getByText } = render(<EventSummary event={{title: 'My Test Event'}} />)
+    const title = getByText('My Test Event')
+    expect(title).toBeInTheDocument()
+})
+
+it('renders event start_datetime', () => {
+    const { getByText } = render(<EventSummary event={{start_datetime: new Date('10 September 2020, 10:00 am')}} />)
+    const start_datetime = getByText('Thu, Sep 10, 2020 10:00 AM')
+    expect(start_datetime).toBeInTheDocument()
+})
+
+it('renders event image', () => {
+    const { getByRole } = render(<EventSummary event={{image_url: 'event.png'}} />)
+    const image = getByRole('img', {src: 'event.png'})
+    expect(image).toBeInTheDocument()
+})
